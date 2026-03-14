952a89dea35b829a789085eeabb0a2ce57c30af7
Den Raskovalov <denbox@gmail.com>
2026-03-13 18:13:44 -0700

    bazel build

--- contrib/libs/libc_compat/string.h
+++ contrib/libs/libc_compat/string.h
@@ -2,7 +2,7 @@
 
 #include <stddef.h>
 #include <ctype.h>
-#include <string.h>
+#include_next <string.h>
 
 #ifdef __cplusplus
 extern "C" {
--- util/stream/file.h
+++ util/stream/file.h
@@ -6,6 +6,7 @@
 #include "buffered.h"
 #include "mem.h"
 
+#include <filesystem>
 #include <util/system/file.h>
 #include <utility>
 
